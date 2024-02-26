24a5f92e7c62acb24a16c2086084cfc4f780dfb1
Lin Khant Htel <linkhanthtel@icloud.com>
2024-02-26 22:50:29 +0800

    "Updated 26th Feb"

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Footer from '../components/footer'
 import { motion } from 'framer-motion'
 
 function About() {
   const [isVisible, setIsVisible] = useState(false);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     setIsVisible(true);
@@ -12,17 +13,39 @@ function About() {
   return (
     <div>
       {/* About Department */}
-      <div className='bg-gray-300 flex flex-col text-center h-full w-screen py-5 md:py-28' >
-        <div className={`transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+      <div 
+      className="flex flex-col text-center h-full w-screen py-5 md:py-28 bg-[url('./assets/image.png')]" >
+        <div
+          className={`transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
+          >
         <h1 className='py-5 text-3xl'>History of Department</h1>
         <p className='px-2 md:px-10 text-lg text-justify'>
-        The Department of Anthropology in Yangon University was established in 1950, in Judson Hall. Its location on the campus has moved over the years: first, to the Social Science building, then in 1964 to Taungoo Hall. Since 1983 the Anthropology Department has been located in Yamanya Hall. The Department started small, with few staff and students but then expanded to include Sociology and even International Relations for a few years until Anthropology was reduced to a minor subject status under the new education system in 1964. At that point, the Department shrank in numbers of staff and students, until 1982 when Anthropology was restored to major subject status again.
-        In the early years of the Department, several overseas academics supported the teaching of Anthropology, Sociology and International Relations in Yangon, while several Myanmar academics furthered their studies in the USA and the UK. Nowadays the Department is staffed by Myanmar academics.
-        Anthropology can be taken by Arts students as well as Science students. Science students taking Anthropology as a minor subject can earn a B.A Degree. Post-graduate students have to carry out field research and collect survey data for their Masters thesis.
-        Following the example of the University of Yangon, in the late 1950s other intermediate colleges teaching Anthropology were opened in Myanmar: in Mawlamyine, Pathein and Kyaukphyu.
-        The Department of Anthropology in Yangon University was established in 1950 under the Rector at that time, Dr. Htin Aung, in Judson Hall. Dr Htin Aung was also an honorary professor of the Anthropology Department, lecturing with visiting professor Dr. K. G. Orr from Chicago University.
-        To begin with the Anthropology Department had seven first year students and three post graduate students. Daw Khin Khin Oo and Daw Khin Hmway were appointed tutors before Dr. K. G. Orr left to go back to Chicago. Then the two tutors were sent on state scholarships to Chicago and Alabama Universities in the USA and Mr. Musgrave lectured during their absence.
-        When Daw Khin Khin Oo and Daw Khin Hmway returned from the USA in the a/y 1952-1953, Daw Khin Khin Oo was appointed Head of the Anthropology Department and the Department started teaching a range of classes from intermediate level, with a Masters degree introduced in 1957.</p>
+        The Department of Anthropology in Yangon University was established in 1950, in Judson Hall. Its location on the campus has moved over the years: first, to the Social Science building, then in 1964 to Taungoo Hall. Since 1983 the Anthropology Department has been located in Yamanya Hall. The Department started small, with few staff and students but then expanded to include Sociology and even International Relations for a few years until Anthropology was reduced to a minor subject status under the new education system in 1964. At that point, the Department shrank in numbers of staff and students, until 1982 when Anthropology was restored to major subject status again. <button onClick={() => setShowMore(prev => !prev)} className=' text-blue-900 px-2'>See More</button></p>
+        
+        { showMore && <p className='px-2 md:px-10 text-lg text-justify'>
+          In the early years of the Department, several overseas academics supported the teaching of Anthropology, Sociology and International Relations in Yangon, while several Myanmar academics furthered their studies in the USA and the UK. Nowadays the Department is staffed by Myanmar academics.
+          Anthropology can be taken by Arts students as well as Science students. Science students taking Anthropology as a minor subject can earn a B.A Degree. Post-graduate students have to carry out field research and collect survey data for their Masters thesis.
+          Following the example of the University of Yangon, in the late 1950s other intermediate colleges teaching Anthropology were opened in Myanmar: in Mawlamyine, Pathein and Kyaukphyu.
+          The Department of Anthropology in Yangon University was established in 1950 under the Rector at that time, Dr. Htin Aung, in Judson Hall. Dr Htin Aung was also an honorary professor of the Anthropology Department, lecturing with visiting professor Dr. K. G. Orr from Chicago University.
+          To begin with the Anthropology Department had seven first year students and three post graduate students. Daw Khin Khin Oo and Daw Khin Hmway were appointed tutors before Dr. K. G. Orr left to go back to Chicago. Then the two tutors were sent on state scholarships to Chicago and Alabama Universities in the USA and Mr. Musgrave lectured during their absence.
+          When Daw Khin Khin Oo and Daw Khin Hmway returned from the USA in the a/y 1952-1953, Daw Khin Khin Oo was appointed Head of the Anthropology Department and the Department started teaching a range of classes from intermediate level, with a Masters degree introduced in 1957.
+          </p>}
+        </div>
+      </div>
+
+      <div className='bg-gray-400 flex flex-col text-center h-full w-screen py-5 md:py-28' >
+        <div className={`transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+        <h1 className='py-5 text-3xl'>Mission and Vision</h1>
+        <div className='px-2 md:px-10 text-lg text-justify'>
+          <ul className='list-disc'>
+            <li>To set up acceptable forms and functions of human diversity and cultures in the present and the past through intensive field work</li>
+            <li>To increase students’ knowledge and skills in the intellectual, social and economic development related to local and global Anthropology</li>
+            <li>To develop creative research, teaching and a leading voice for the practice of public, collaborative and community engaged Anthropology</li>
+            <li>To study bio-cultural diversity in the present and the past</li>
+            <li>To develop students’ skills in research and theories, writing and the application of Anthropological knowledge to enhance academic excellence and professional development</li>
+            <li>To offer the highest quality instruction in Anthropological implementation in community development</li>
+          </ul>
+        </div>
         </div>
       </div>
 
@@ -30,7 +53,7 @@ function About() {
       <div className='bg-white flex flex-row text-start h-full w-screen py-5 md:py-28'>
         <div className={`transition-opacity duration-1000 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
         <h1 className='py-5 text-3xl md:px-10 text-center md:w-[50%]'>Anthropology Museum</h1>
-        <p className='text-center md:px-10 md:text-start md:w-[50%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis possimus praesentium incidunt iste. Aspernatur quia ad ipsa ullam eaque. Distinctio consectetur perferendis quas, voluptatem magnam voluptate voluptates nihil soluta ipsum.</p>
+        <p className='text-center text-lg md:px-10 md:text-start md:w-[50%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis possimus praesentium incidunt iste. Aspernatur quia ad ipsa ullam eaque. Distinctio consectetur perferendis quas, voluptatem magnam voluptate voluptates nihil soluta ipsum.</p>
         </div>
         <div>
           <h1 className='px-10 hidden md:flex'>Image</h1>
--- src/pages/program.jsx
+++ src/pages/program.jsx
@@ -1,71 +1,37 @@
-import { PaperClipIcon } from '@heroicons/react/20/solid'
 
 export default function Program() {
   return (
     <div>
       <div className="px-4 my-5 sm:px-0">
         <h1 className="text-base font-semibold leading-7 text-center text-gray-900">Course Information</h1>
-        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
+        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Course details and information</p>
       </div>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+            <dt className="text-sm font-medium leading-6 text-gray-900">B.A</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">First Year, Second Year, Third Year, Final Year</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Application for</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Backend Developer</dd>
+            <dt className="text-sm font-medium leading-6 text-gray-900">B.A (Hons)</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">First Year, Second Year, First Year (Hons), Second Year (Hons), Third Year (Hons)</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+            <dt className="text-sm font-medium leading-6 text-gray-900">M.A</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">M.A (First Year), M.A (Second Year)</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Salary expectation</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
+            <dt className="text-sm font-medium leading-6 text-gray-900">Ph. D</dt>
+            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">GRADUATE COURSES FOR Ph. D. IN ANTHROPOLOGY <br />
+            Total Credits = 16 per week <br />
+            Total Hours   = 28 per week</dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">About</dt>
+            <dt className="text-sm font-medium leading-6 text-gray-900">Post-graduate Diploma</dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-              qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
-              pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
-            <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">resume_back_end_developer.pdf</span>
-                      <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">coverletter_back_end_developer.pdf</span>
-                      <span className="flex-shrink-0 text-gray-400">4.5mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-              </ul>
+            COURSES FOR DIPLOMA IN ANTHROPOLOGY <br />
+            Total Credits = 16 per week <br />
+            Total Hours =   20 per week
             </dd>
           </div>
         </dl>
